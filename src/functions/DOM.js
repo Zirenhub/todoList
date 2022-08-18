5e469dbef1b8df7fb90488e5972b9cb96e331983
Erdinch <erdinchorhan02@gmail.com>
2022-08-18 22:35:18 +0300

    DOM

---
+++ src/functions/DOM.js
@@ -0,0 +1,7 @@
+export let DOM = {
+  sideButtons: document.querySelectorAll('.side-buttons'),
+  addProjectButton: document.querySelector('#addProjects'),
+  projectContainer: document.querySelector('.side-projects'),
+  mainPage: document.querySelector('.tasks-container'),
+  pageTitle: document.querySelector('#page-title'),
+};
