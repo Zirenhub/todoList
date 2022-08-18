fa00503923639ddbd0e43897a8c42b6ebb2eb3a1
Erdinch <erdinchorhan02@gmail.com>
2022-08-18 22:35:10 +0300

    main app

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import { DOM } from './functions/DOM';
+import { changeTitleImportant } from './pages/importantPage';
+import { changeTitleTasks } from './pages/allTasksPage';
+import { changeTitleToday } from './pages/todayPage';
+import { createNewProject } from './functions/projectCreate';
+
+let page = {
+  changePage: function (buttonText) {
+    if (buttonText === 'Important') {
+      changeTitleImportant(DOM.pageTitle);
+    } else if (buttonText === 'All Tasks') {
+      changeTitleTasks(DOM.pageTitle);
+    } else {
+      changeTitleToday(DOM.pageTitle);
+    }
+  },
+
+  bindEvents: function () {
+    DOM.sideButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        let buttonText = button.textContent;
+        this.changePage(buttonText);
+      });
+    });
+    DOM.addProjectButton.addEventListener('click', () => {
+      createNewProject();
+    });
+  },
+};
+
+page.bindEvents();
