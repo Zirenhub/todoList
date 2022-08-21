dc1c8f15814bac71a8e3754f38203fd3692b4868
Erdinch <erdinchorhan02@gmail.com>
2022-08-21 19:14:29 +0300

    DOM delete btn

--- src/functions/DOM.js
+++ src/functions/DOM.js
@@ -3,6 +3,9 @@ export let DOM = {
   projectContainer: document.querySelector('.side-projects'),
   addProjectButton: document.querySelector('#addProjects'),
   projectButtons: document.querySelectorAll('.project-button'),
+  projectDelButtons: document.querySelectorAll(
+    '.project-delete-button'
+  ),
   mainPage: document.querySelector('.tasks-container'),
   pageTitle: document.querySelector('#page-title'),
 };
