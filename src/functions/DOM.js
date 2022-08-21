94f10babc16e40b91a2fda941baaea36b95df9b9
Erdinch <erdinchorhan02@gmail.com>
2022-08-21 17:04:35 +0300

    DOM restore

---
+++ src/functions/DOM.js
@@ -0,0 +1,8 @@
+export let DOM = {
+  sideButtons: document.querySelectorAll('.side-buttons'),
+  projectContainer: document.querySelector('.side-projects'),
+  addProjectButton: document.querySelector('#addProjects'),
+  projectButtons: document.querySelectorAll('.project-button'),
+  mainPage: document.querySelector('.tasks-container'),
+  pageTitle: document.querySelector('#page-title'),
+};
