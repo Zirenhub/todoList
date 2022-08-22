ea7defea887b0f4f6bafa84ed04b21ce253529d9
Erdinch <erdinchorhan02@gmail.com>
2022-08-22 19:13:19 +0300

    working change page system

--- src/app.js
+++ src/app.js
@@ -2,24 +2,38 @@ import './style.css';
 import { DOM } from './functions/DOM';
 import { createNewProject } from './functions/projectCreate';
 
+let allTasksPage = () => {
+  if (DOM.pageTitle.textContent === 'All Tasks') {
+    return;
+  }
+  let mainPage = document.querySelector('.main-todo-container');
+
+  DOM.pageTitle.textContent = 'All Tasks';
+
+  const allTasksPageContent = document.createElement('div');
+  allTasksPageContent.classList.add('all-tasks-page-container');
+
+  mainPage.replaceWith(allTasksPageContent);
+};
+
 let page = {
-  // changePage: function (buttonText) {
-  //   if (buttonText === 'Important') {
-  //     changeTitleImportant(DOM.pageTitle);
-  //   } else if (buttonText === 'All Tasks') {
-  //     changeTitleTasks(DOM.pageTitle);
-  //   } else {
-  //     changeTitleToday(DOM.pageTitle);
-  //   }
-  // },
+  changePage: function (buttonText) {
+    if (buttonText === 'Important') {
+      // TODO
+    } else if (buttonText === 'All Tasks') {
+      allTasksPage();
+    } else {
+      changeTitleToday(DOM.pageTitle);
+    }
+  },
 
   bindEvents: function () {
-    // DOM.sideButtons.forEach((button) => {
-    //   button.addEventListener('click', () => {
-    //     let buttonText = button.textContent;
-    //     this.changePage(buttonText);
-    //   });
-    // });
+    DOM.sideButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        let buttonText = button.textContent;
+        this.changePage(buttonText);
+      });
+    });
     DOM.addProjectButton.addEventListener('click', () => {
       if (document.querySelector('.new-task-field')) {
         document.querySelector('.new-task-input').focus();
