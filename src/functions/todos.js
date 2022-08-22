4069194fb25ec71ca0f588183da9d0fddd8f1fc0
Erdinch <erdinchorhan02@gmail.com>
2022-08-22 18:24:04 +0300

    changeName,Desc,Date addition

--- src/functions/todos.js
+++ src/functions/todos.js
@@ -9,4 +9,19 @@ export default class toDoCons {
   createPage() {
     return this.toDoContainer;
   }
+
+  changeName(title) {
+    this.title = title;
+    return this.title;
+  }
+
+  changeDescription(description) {
+    this.description = description;
+    return this.description;
+  }
+
+  changeDate(date) {
+    this.date = date;
+    return this.date;
+  }
 }
