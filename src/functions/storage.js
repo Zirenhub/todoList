010d5c9efe896dc29e93b09779f5740e14252d6a
Erdinch <erdinchorhan02@gmail.com>
2022-08-24 18:51:48 +0300

    deleted

--- src/functions/storage.js
+++
@@ -1,9 +0,0 @@
-import { projects } from './projectCreate';
-
-class Storage {
-  static saveProjects() {
-    localStorage.setItem('projectsList', JSON.stringify(projects));
-  }
-}
-
-export { Storage };
